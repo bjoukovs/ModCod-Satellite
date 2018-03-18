05c1e3f82046cdce4fce050c82438ee9819b264d
bjoukovs <borisjoukov@hotmail.com>
2018-03-18 18:04:17 +0100

    demapping

--- test_mapping_demapping.m
+++ test_mapping_demapping.m
@@ -15,3 +15,9 @@ symb_tx = mapping(bits,bits_per_symbol,modulation)
 
 
 %% DEMAPPING
+
+bits_rx = demapping(symb_tx,bits_per_symbol,modulation)
+
+%% Check error
+
+error_norm = norm(bits_rx - bits)
