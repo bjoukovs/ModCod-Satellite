9ee828949853a42788eb5bc70d3333a715aebbe1
Laurent-St <laurent-st@hotmail.com>
2018-04-17 17:28:02 +0200

    hard_decoder NE MARCHE PAS POUR 1-->0

--- LDPC/Encoder_small.m
+++ LDPC/Encoder_small.m
@@ -25,7 +25,14 @@ G = [Pt' I]
 mod(G*H', 2) %check property
 
 bits = randi(2,[1,5]) - ones(1,5)
-code = mod(bits*G,2)
+code = [1     1     1     0     0     0     0     0     1     0]
+%mod(bits*G,2)
+code = [1     1     1     0     1     0     0     0     1     0]
+%mod(code(5)+1,2)
+received = hard_decoder(H,code)
+
+%ATTENTION ne marche que si l'erreur a converti un bit 1 en bit 0, pas dans
+%l'autre sens --> A CORRIGER
 
 
 
